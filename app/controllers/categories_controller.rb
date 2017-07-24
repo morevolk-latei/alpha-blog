50f07fede0e8814fb2930cdeefec98bdc9afd235
Rizul Sharma <rishant46sharma@gmail.com>
2017-07-24 19:43:40 +0530

    Added edit category link

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-    before_action :require_admin, only: [:new, :create]
+    before_action :require_admin, only: [:new, :create, :edit]
     def index
         @categories = Category.paginate(page: params[:page], per_page: 10)
     end
@@ -20,6 +20,21 @@ class CategoriesController < ApplicationController
             redirect_to categories_path
         else
             render 'new'
+        end
+    end
+
+    def edit
+
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category name was successfuly updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
         end
     end
 
